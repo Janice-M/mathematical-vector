b59b7931fa0b734c6683cfce209cbef5501e67bc
Janice-M <janiceink001@gmail.com>
2021-07-20 01:56:46 +0300

    function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /* create a class  for the constructor */
 class Vector {
-    constructor(x,y) {
-      this._x = x;
-      this._y = y;
+    constructor(a,b) {
+      this._a= a;
+      this._b = b;
     }
   }
+  /* get the legnth of a vector which is the square root of the sum of the squares of the horizontal and vertical components */
